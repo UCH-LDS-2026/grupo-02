862589ef6caaf61de46d6815453e7e3e4eb675ed
JNahuelGorriG <130795354+JNahuelGorriG@users.noreply.github.com>
2026-05-08 22:08:06 -0300

    Add files via upload

---
+++ src/test/java/grupo2/restosoft/RestosoftApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.restosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestosoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
