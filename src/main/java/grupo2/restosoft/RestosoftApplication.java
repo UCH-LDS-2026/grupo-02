0ef5a3b84a362aae0a8231ce3c7f283902292e4b
JNahuelGorriG <130795354+JNahuelGorriG@users.noreply.github.com>
2026-05-08 22:03:48 -0300

    Add files via upload

---
+++ src/main/java/grupo2/restosoft/RestosoftApplication.java
@@ -0,0 +1,13 @@
+package grupo2.restosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestosoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestosoftApplication.class, args);
+	}
+
+}
